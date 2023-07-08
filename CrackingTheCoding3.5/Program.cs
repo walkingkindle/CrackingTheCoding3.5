d86379fe0002792d2cc634ccdf7417186f0cc316
walkingkindle <122030667+walkingkindle@users.noreply.github.com>
2023-07-08 12:52:45 +0200

    Add project files.

---
+++ CrackingTheCoding3.5/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Solution
+{
+    internal class StackSurgeries
+    {
+
+
+
+        public Stack<int> sortStackSmallest(Stack<int> inputStack)
+        {
+            Stack<int> tmpStack = new Stack<int>();
+
+
+            while(inputStack != null)
+            {
+                var temp = inputStack.Pop();
+
+                while (tmpStack != null && tmpStack.Peek() < temp)
+                {
+                    inputStack.Push(tmpStack.Pop());
+                }
+
+                tmpStack.Push(temp);
+
+            }
+
+            return tmpStack;
+
+
+        }
+        
+
+
+
+
+        }
+
+
+
+    }
+
+
+}
